d44f817b838a32c2efedb36e731623ffaaf54e04
Peter DV <pdevroom@outlook.com>
2022-12-01 18:10:54 +1100

    Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+namespace Day1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Part one
+            var input = File.ReadAllLines("input.txt");
+            List<int> totals = new();
+            totals.Add(0);
+            var sum = 0;
+            foreach (var line in input)
+            {
+                if (line == "")
+                {
+                    totals.Add(sum);
+                    sum = 0;
+                    continue;
+                }
+                sum += Int32.Parse(line);
+            }
 
-Console.WriteLine("Hello, World!");
+            var biggestTotal = totals.Max();
+            Console.WriteLine($"Part One -- {biggestTotal}");
+            
+            // Part two
+            totals.Sort();
+            var sumOfTopThree = totals[^1] + totals[^2] + totals[^3];
+            Console.WriteLine($"Part Two -- {sumOfTopThree}");
+
+        }
+    }
+}
